dcb3f303901061b32c1576def890ea4379de8270
Patrick Capps <patrick.capps0@gmail.com>
2019-01-19 12:35:59 -0500

    added ball, script changes color to red

--- Assets/Scripts/testScript.cs
+++ Assets/Scripts/testScript.cs
@@ -6,7 +6,7 @@ public class testScript : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
+        GetComponent<Renderer>().material.color = Color.red;
 	}
 	
 	// Update is called once per frame
