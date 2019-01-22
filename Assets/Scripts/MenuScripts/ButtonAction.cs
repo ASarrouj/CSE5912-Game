5fe3c6ee24cd157b7032e0947e02eb7bc8083632
Ian Pennington <irpennin@gmail.com>
2019-01-21 20:53:07 -0500

    Added basic main menu
    New menu options should be simple to add, and the design can be changed later depending on team input.

---
+++ Assets/Scripts/MenuScripts/ButtonAction.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonAction : MonoBehaviour
+{
+    public enum Action { Play, Quit };
+    public Action action;
+    public AudioClip soundEffect;
+    public AudioSource source;
+
+    private bool ignore = false;
+
+    void OnMouseUp() {
+        if (!ignore) {
+            ignore = true;
+            TakeAction();
+        }
+    }
+
+    public void TakeAction() {
+        ignore = true;
+        switch (action) {
+            case Action.Play:
+                StartCoroutine(Play());
+                break;
+            case Action.Quit:
+                QuitGame();
+                break;
+            default:
+                break;
+        }
+    }
+
+    IEnumerator Play() {
+        source.PlayOneShot(soundEffect);
+        GameObject blackScreen = GameObject.CreatePrimitive(PrimitiveType.Plane);
+        blackScreen.transform.SetPositionAndRotation(new Vector3(0, 0, -5), Quaternion.Euler(270, 0, 0));
+        blackScreen.transform.localScale = new Vector3(1.8f, 1, 1.2f);
+        blackScreen.layer = 1;
+        Material m = blackScreen.GetComponent<Renderer>().material;
+        m.SetFloat("_Mode", 3);
+        m.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.One);
+        m.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
+        m.SetInt("_ZWrite", 0);
+        m.DisableKeyword("_ALPHATEST_ON");
+        m.DisableKeyword("_ALPHABLEND_ON");
+        m.EnableKeyword("_ALPHAPREMULTIPLY_ON");
+        m.renderQueue = 3000;
+        for (float i = 0f; i <= 3; i += Time.deltaTime) {
+            m.color = Color.Lerp(Color.clear, Color.black, i / 3);
+            yield return null;
+        }
+        SceneManager.LoadScene("GameScene");
+    }
+
+    private void QuitGame() {
+        source.PlayOneShot(soundEffect);
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/MenuScripts/EventManager.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.Events;
+
+public class EventManager : MonoBehaviour
+{
+    public AudioClip menuSound;
+    public GameObject[] buttons;
+    public Color selectColor = Color.red;
+    public Color defaultColor = Color.white;
+
+    private AudioSource source;
+    private float hor;
+    private float vert;
+    private int currentButton;
+    private bool ignore = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentButton = 0;
+        SetButton(currentButton);       
+        source = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!ignore) {
+            hor = Input.GetAxis("Horizontal");
+            vert = Input.GetAxis("Vertical");
+            HandleButtonSelection();
+
+            if (Input.GetButtonDown("Submit")) {
+                ignore = true;
+                EventSystem.current.currentSelectedGameObject.SendMessage("TakeAction");
+            }
+        }
+    }
+    
+    private void HandleButtonSelection() {
+        if (hor == 0 && vert == 0) {
+            for (int i = 0; i < buttons.Length; i++) {
+                if (buttons[i].GetComponent<Renderer>().material.color == selectColor) {
+                    EventSystem.current.SetSelectedGameObject(buttons[i]);
+                    currentButton = i;
+                    SetButton(currentButton);
+                    break;
+                }
+            }
+            return;
+        } else if (hor > 0 || vert > 0) {
+            ScrollPrev();
+        } else if (hor < 0 || vert < 0) {
+            ScrollNext();
+        }
+
+        if (EventSystem.current.currentSelectedGameObject != null) {
+            foreach (GameObject b in buttons) {
+                clearButton(b);
+            }
+            highlightButton(EventSystem.current.currentSelectedGameObject);
+        }
+    }
+
+    private void ScrollNext() {
+        if (currentButton < buttons.Length - 1) {
+            currentButton++;
+            SetButton(currentButton);
+            source.PlayOneShot(menuSound);
+        }
+    }
+
+    private void ScrollPrev() {
+        if (currentButton > 0) {
+            currentButton--;
+            SetButton(currentButton);
+            source.PlayOneShot(menuSound);
+        }
+    }
+
+    private void SetButton(int index) {
+        EventSystem.current.SetSelectedGameObject(buttons[index]);
+    }
+
+    private void highlightButton(GameObject button) {
+        button.GetComponent<Renderer>().material.color = selectColor;
+    }
+
+    private void clearButton(GameObject button) {
+        button.GetComponent<Renderer>().material.color = defaultColor;
+    }
+}
