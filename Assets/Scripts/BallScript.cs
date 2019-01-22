0d2051d833f4be00cb077f9fe45ee547ef0728aa
Nar Wang <narlynarx@gmail.com>
2019-01-22 13:20:18 -0500

    Testing ball script

---
+++ Assets/Scripts/BallScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    bool startLeft;
+    Vector3 vel;
+    void Start()
+    {
+        startLeft=true;
+        vel=new Vector3(-0.1f,0,0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    transform.Translate(vel);
+        if(transform.position.x>10f||transform.position.x<-10f)
+        {
+            transform.position=new Vector3(0f,0.5f,0f);
+        }
+    }
+}
