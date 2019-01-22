46fb4f9081574622c0cf25f7cc75737d7c1aeaa3
Nar Wang <narlynarx@gmail.com>
2019-01-22 12:55:13 -0500

    Adding simple pong feature

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.S)&&transform.position.z>-5f)
+        {
+            transform.Translate(0,0,-.1f);
+        }
+        else if(Input.GetKey(KeyCode.W)&&transform.position.z<5f)
+        {
+            transform.Translate(0,0,.1f);
+        }
+    }
+}
