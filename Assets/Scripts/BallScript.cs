fdc559b1d21e1724e204465683ee742533c0b6a5
Nar Wang <narlynarx@gmail.com>
2019-01-22 17:09:45 -0500

    Collision Tester

--- Assets/Scripts/BallScript.cs
+++ Assets/Scripts/BallScript.cs
@@ -6,25 +6,55 @@ public class BallScript : MonoBehaviour
 {
     // Start is called before the first frame update
     bool startLeft;
+    GameObject player1,player2;
     Vector3 vel;
     void Start()
     {
         startLeft=true;
         vel=new Vector3(-0.1f,0,-.1f);
+        player1=GameObject.Find("Player1");
+        player2=GameObject.Find("Player2");
     }
 
     // Update is called once per frame
     void Update()
     {
-    transform.Translate(vel);
-        if(transform.position.x>10f||transform.position.x<-10f)
+        if(transform.position.x>8.8f||transform.position.x<-8.8f)
         {
-            transform.position=new Vector3(0f,0.5f,0f);
+            checkCollision();
         }
-        if(transform.position.z>5f||transform.position.z<-5f)
+        else if(transform.position.z>5f||transform.position.z<-5f)
         {
-            vel=new Vector3(vel.z,0,-vel.x);
+            vel=new Vector3(vel.x,0,-vel.z);
         }
+        transform.Translate(vel);
         
+    }
+    void checkCollision()
+    {
+         if(transform.position.x>8.8f)
+         {
+             float dist=player2.transform.position.z-transform.position.z;
+             if(dist<.75f||dist>-.75f)
+             {
+                 float ballX=-.141f*Mathf.Cos((dist*100)*Mathf.PI/180);
+                 float ballZ=.141f*-Mathf.Sin((dist*100)*Mathf.PI/180);
+                 vel=new Vector3(ballX,0,ballZ);
+             }
+         }
+        else if(transform.position.x<-8.8f)
+         {
+             float dist=player1.transform.position.z-transform.position.z;
+             if(dist<.75f||dist>-.75f)
+             {
+                 float ballX=.141f*Mathf.Cos((dist*100)*Mathf.PI/180);
+                 float ballZ=.141f*-Mathf.Sin((dist*100)*Mathf.PI/180);
+                 vel=new Vector3(ballX,0,ballZ);
+             }
+         }
+        else
+        {
+             transform.position=new Vector3(0f,0.5f,0f);
+        }
     }
 }
