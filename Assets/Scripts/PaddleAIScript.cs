cb9968401cca84c36b5bbec37f321ee4032526eb
ASarrouj <asarrouj590@gmail.com>
2019-01-23 03:21:05 -0500

    added player 2 paddle AI

---
+++ Assets/Scripts/PaddleAIScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleAIScript : MonoBehaviour
+{
+    private Transform ball;
+    private float speed;
+    private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        ball = GameObject.Find("Sphere").transform;
+        speed = 1.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 movement = (ball.position - transform.position).normalized;
+        movement.x = 0.0f;
+        movement.y = 0.0f;
+
+        transform.Translate(movement * speed);
+    }
+}
