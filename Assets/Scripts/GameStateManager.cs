01e75f62ddeed52c77accfa9798f756fe07295a9
Tengri Zhang <zhang.5863@osu.edu>
2019-01-23 16:39:48 -0500

    Loading Bar Update

--- Assets/Scripts/GameStateManager.cs
+++ Assets/Scripts/GameStateManager.cs
@@ -7,7 +7,7 @@ public class GameStateManager : MonoBehaviour
     //num = 0: StartState
     //num = 1: Loading State
     //num = 2: Main Menu State
-    //num = 3: Play Game State
+    //num = 3: Play Pong State
     public static void LoadState(int num) {
 
         LoadingScreenManager.LoadScene(num);
