4befe64637025ed2f309fe27384d0c52aed30372
Nar Wang <narlynarx@gmail.com>
2019-02-01 03:14:02 -0500

    Adding in Classes for now

---
+++ Assets/Scripts/ProjectileScripts/ProjectileShoot.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileShoot : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Camera gunCamera;
+    void Start()
+    {
+            gunCamera = GetComponentInParent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
