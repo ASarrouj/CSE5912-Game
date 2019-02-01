c4a4f10d754831691de2884f8206707c81412a19
kunkudunk <46832170+kunkudunk@users.noreply.github.com>
2019-02-01 11:26:06 -0500

    changed script names and added more to mech scene
    Added a second mech to mech scene. Each mech has 3 gun mounts and the method to add guns during runtime is coded.

---
+++ Assets/Scripts/MechComp.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MechComp : MonoBehaviour
+{
+    public GunInterface[] guns;
+    public Transform[] positions;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void AttachGun(GunInterface gun, int location)
+    {
+        if(location < guns.Length)
+        {
+            guns[location] = gun;
+            guns[location].transform.position = positions[location].transform.position;
+            guns[location].transform.rotation = positions[location].transform.rotation;
+        }
+        else
+        {
+            print("This location is not available on the mech.");
+        }
+    }
+}
