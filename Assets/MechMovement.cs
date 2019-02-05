8e7b16a6ff3ec15707105d9308e7f4cec9d69fe5
Tengri Zhang <zhang.5863@osu.edu>
2019-02-04 22:59:18 -0500

    Basic mech movement

---
+++ Assets/MechMovement.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MechMovement : MonoBehaviour
+{
+    private int moveSpeed;
+    private int rotateSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        moveSpeed = 0;
+        rotateSpeed = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow) && moveSpeed<3)
+        {
+            moveSpeed++;
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow) && moveSpeed >-1)
+        {
+            moveSpeed--;
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow) && moveSpeed != 0 && rotateSpeed >= -30 && rotateSpeed <= 30)
+        {
+            if (moveSpeed > 0 && rotateSpeed > -30)
+            {
+                rotateSpeed -= 5;
+            }
+            else if (moveSpeed < 0 && rotateSpeed < 30)
+            {
+                rotateSpeed += 5;
+            }
+
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow) && moveSpeed != 0 && rotateSpeed <= 30 && rotateSpeed >= -30)
+        {
+            if (moveSpeed > 0 && rotateSpeed < 30)
+            {
+                rotateSpeed += 5;
+            }
+            else if (moveSpeed < 0 && rotateSpeed > -30)
+            {
+                rotateSpeed -= 5;
+            }
+        }
+        if (moveSpeed == 0)
+        {
+            rotateSpeed = 0;
+        }
+        transform.Translate(0, 0, Time.deltaTime * moveSpeed, Space.Self);
+        transform.Rotate(0, Time.deltaTime * rotateSpeed, 0, Space.Self);
+    }
+}
