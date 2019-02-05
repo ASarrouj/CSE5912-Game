ced5715806a2f3a0b309e082d50f8304549d1aa7
Nar Wang <narlynarx@gmail.com>
2019-02-05 12:41:34 -0500

    Projectiles Version 2, disappearing

--- Assets/Scripts/Weapons/ProjectileScripts/Projectile.cs
+++ Assets/Scripts/Weapons/ProjectileScripts/Projectile.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
+    public float timer=0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,10 @@ public class Projectile : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        timer+=Time.deltaTime;
+        if(timer>10.0f)
+        {
+            Destroy(this.gameObject);
+        }
     }
 }
