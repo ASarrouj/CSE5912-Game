0519f421067bbfb8bfcf8a0247a7e9b29dac32c0
Nar Wang <31118588+narlynar@users.noreply.github.com>
2019-02-06 04:30:19 -0500

    Cannon Sounds

---
+++ Assets/Scripts/Weapons/ShootInput2.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootInput2 : MonoBehaviour
+{
+    public GameObject currentGun;
+
+    private IWeapon gunBehavior;
+    public AudioClip aud;
+    AudioSource source;
+
+    private void Awake()
+    {
+        gunBehavior = currentGun.GetComponent<IWeapon>();
+        source=GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       if (Input.GetButton("Fire1"))
+        {
+            gunBehavior.Shoot();
+            source.PlayOneShot(aud,.3f);
+        }
+    }
+}
