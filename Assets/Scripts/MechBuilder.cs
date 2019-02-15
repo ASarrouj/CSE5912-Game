5a723d5ad101df830258d7a36f31761ca2b60e61
kunkudunk <46832170+kunkudunk@users.noreply.github.com>
2019-02-15 17:29:37 -0500

    Foundations for mechConstructionUI  at runtime
    Added script and some UI elements for attaching guns to the mech at runtime. Buttons for attaching the guns not yet in the UI, logic for switching between the screens exists in the script for the UI.

---
+++ Assets/Scripts/MechBuilder.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MechBuilder : MonoBehaviour
+{
+
+    public GameObject[] mechPrefabs;
+    public GameObject[] partScreens;
+    public GameObject mechScreen;
+
+    public void moveToGunScreen(int screen)
+    {
+        mechScreen.SetActive(false);
+        partScreens[screen].SetActive(true);
+    }
+
+    public void backToMechScreen(int current)
+    {
+        partScreens[current].SetActive(false);
+        mechScreen.SetActive(true);
+    }
+}
