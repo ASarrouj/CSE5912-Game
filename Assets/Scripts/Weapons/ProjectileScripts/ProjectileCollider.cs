6e3a356dcb303c4a388c2a9041bc7dbb1ab7dcd0
Nar Wang <31118588+narlynar@users.noreply.github.com>
2019-02-21 19:54:05 -0500

    Changed projectile collider

--- Assets/Scripts/Weapons/ProjectileScripts/ProjectileCollider.cs
+++ Assets/Scripts/Weapons/ProjectileScripts/ProjectileCollider.cs
@@ -20,21 +20,30 @@ public class ProjectileCollider : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("Target"))
         {
-            collision.gameObject.GetComponent<TargetTakeDamage>().Damage(damage);
+            collision.gameObject.GetComponent<IDamagable>().Damage(damage);
             Instantiate(explosion,transform.position, transform.rotation);
             GameObject sound=GameObject.Find("SoundManager");
             AudioSource source=sound.GetComponent<AudioSource>();
             source.PlayOneShot(clip,1f);
-            Destroy(gameObject);
+            this.gameObject.SetActive(false);
         }
         if (collision.gameObject.CompareTag("Mech"))
         {
-            collision.gameObject.GetComponent<TargetTakeDamage>().Damage(damage);
+            collision.gameObject.GetComponent<IDamagable>().Damage(damage);
             Instantiate(explosion,transform.position, transform.rotation);
             GameObject sound=GameObject.Find("SoundManager");
             AudioSource source=sound.GetComponent<AudioSource>();
             source.PlayOneShot(clip,1f);
-            Destroy(gameObject);
+            this.gameObject.SetActive(false);
+        }
+        if (collision.gameObject.CompareTag("Building"))
+        {
+            collision.gameObject.GetComponent<IDamagable>().Damage(damage);
+            Instantiate(explosion,transform.position, transform.rotation);
+            GameObject sound=GameObject.Find("SoundManager");
+            AudioSource source=sound.GetComponent<AudioSource>();
+            source.PlayOneShot(clip,1f);
+            this.gameObject.SetActive(false);
         }
         if (collision.gameObject.layer==8)
         {
@@ -42,7 +51,7 @@ public class ProjectileCollider : MonoBehaviour
             GameObject sound=GameObject.Find("SoundManager");
             AudioSource source=sound.GetComponent<AudioSource>();
             source.PlayOneShot(clip,1f);
-            Destroy(gameObject);
+            this.gameObject.SetActive(false);
         }
     }
 }
