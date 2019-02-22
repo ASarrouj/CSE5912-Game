89469aaeccd9e2e85ff879c78f2656987ca7ba88
clinton-23 <35583413+clinton-23@users.noreply.github.com>
2019-02-21 21:15:17 -0500

    add standalone server autostart capability

---
+++ Assets/StandaloneServerAutostart.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class StandaloneServerAutostart : MonoBehaviour
+{
+    bool started;
+    NetworkManager netManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        started = false;
+        netManager = GameObject.Find("NetworkManager").GetComponent<NetworkManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+#if UNITY_SERVER
+        if (!started)
+        {
+            Debug.Log("Server build, hosting game now.");
+            started = true;
+            netManager.StartHost();    
+        }
+#endif
+    }
+}
