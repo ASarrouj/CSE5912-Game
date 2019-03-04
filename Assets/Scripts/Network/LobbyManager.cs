dc63860dbbc65d79f0afa0e5b2450c9b3895474a
Patrick Capps <patrick.capps0@gmail.com>
2019-03-03 22:36:53 -0500

    buttons update match name / network address

--- Assets/Scripts/Network/LobbyManager.cs
+++ Assets/Scripts/Network/LobbyManager.cs
@@ -2,12 +2,36 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
+using UnityEngine.UI;
 
 public class LobbyManager : NetworkLobbyManager
 {
+
+    private Text lobbyTitle;
+    private InputField matchNameField;
+    private InputField ipAddress;
+
+    private void Start()
+    {
+        lobbyTitle = GameObject.Find("LobbyTitle").GetComponent<Text>();
+        matchNameField = GameObject.Find("MatchName").GetComponent<InputField>();
+        ipAddress = GameObject.Find("IPAddress").GetComponent<InputField>();
+    }
+
     public void CreateMatch()
     {
+        if (matchNameField.text != "")
+        {
+            singleton.matchName = matchNameField.text;
+            lobbyTitle.text = singleton.matchName;
+        }
         singleton.StartHost();
+    }
+
+    public void JoinMatch()
+    {
+        singleton.networkAddress = ipAddress.text;
+        singleton.StartClient();
     }
 }
 
