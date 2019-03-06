2249b717cf4e24a2d55634d955fe6648d5180695
Tengri Zhang <zhang.5863@osu.edu>
2019-03-06 13:00:35 -0500

    Health bar sync with player health

---
+++ Assets/Scripts/UI/HealthBar.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthBar : MonoBehaviour
+{
+    private Transform bar;
+    // Start is called before the first frame update
+    void Start()
+    {
+        bar = transform;
+    }
+
+    public void setHealthBar(float health) {
+        bar.localScale = new Vector3(health, 1f);
+    }
+
+}
