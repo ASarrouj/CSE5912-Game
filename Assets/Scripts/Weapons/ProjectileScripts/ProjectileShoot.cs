aaf91c0c38cd04d1236a9f3908aad0e952377872
Patrick Capps <patrick.capps0@gmail.com>
2019-03-06 16:43:06 -0500

    meta deletions and projectile line renderer fix

--- Assets/Scripts/Weapons/ProjectileScripts/ProjectileShoot.cs
+++ Assets/Scripts/Weapons/ProjectileScripts/ProjectileShoot.cs
@@ -18,7 +18,7 @@ public class ProjectileShoot : MonoBehaviour, IWeapon
     void OnEnable()
     {
         source = GetComponent<AudioSource>();
-        line = transform.Find("WeaponUI").GetComponent<LineRenderer>();
+        line = GetComponentInChildren<LineRenderer>();
         roundPool = GameObject.Find("ArtProjectilePool").GetComponent<ObjectPooler>();
     }
 
