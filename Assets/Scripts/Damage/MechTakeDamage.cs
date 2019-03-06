ba39c7cb786091d71efb3e5391af5468fad6a37c
Nar Wang <31118588+narlynar@users.noreply.github.com>
2019-03-06 03:06:59 -0500

    Reformatted everything about health

--- Assets/Scripts/Damage/MechTakeDamage.cs
+++ Assets/Scripts/Damage/MechTakeDamage.cs
@@ -8,9 +8,8 @@ public class MechTakeDamage : NetworkBehaviour, IDamagable
     public bool Invincible;
     public enum Hitbox { FrontHitbox, LeftHitbox, RightHitbox, RearHitbox, CoreHitbox}
     public Hitbox hitboxType;
-    public const int maxHealth=30;
     private bool coreDestroyed;
-    [SyncVar] public int health = 30;
+    public int health=30;
     [SerializeField] GameObject [] particleEffects;
     // Start is called before the first frame update
     void Start()
@@ -31,40 +30,41 @@ public class MechTakeDamage : NetworkBehaviour, IDamagable
         if (hitboxType == Hitbox.FrontHitbox)
         {
             Debug.Log("Front takes damage");
-            health -= dmgAmount;
-            if (health <= 0 && particleEffects != null && particleEffects.Length > 0)
-            {
-                RpcExplodingFront();
-                // GameObject explosion = Instantiate(particleEffects[0], transform.position, Quaternion.identity);
-                // explosion.transform.localScale -= new Vector3(1f, 1f, 1f);
-                // Destroy(explosion, 3f);
-                // Destroy(transform.parent.gameObject);
-            }
+            transform.parent.parent.GetComponent<PlayerHealth>().dmgFront(dmgAmount,this);
+            // if (health <= 0 && particleEffects != null && particleEffects.Length > 0)
+            // {
+            //     RpcExplodingFront();
+            //     // GameObject explosion = Instantiate(particleEffects[0], transform.position, Quaternion.identity);
+            //     // explosion.transform.localScale -= new Vector3(1f, 1f, 1f);
+            //     // Destroy(explosion, 3f);
+            //     // Destroy(transform.parent.gameObject);
+            // }
         }
 
         if (hitboxType == Hitbox.LeftHitbox)
         {
             Debug.Log("Left takes damage");
 
-            health -= dmgAmount;
-            if (health <= 0 && /*particleEffects != null &&*/particleEffects.Length > 0)
-            {
-                RpcExploding();
-                // GameObject explosion = Instantiate(particleEffects[0], transform.position, Quaternion.identity);
-                // Destroy(transform.parent.gameObject);
-                // Destroy(explosion, 3f);
-            }
+                        transform.parent.parent.GetComponent<PlayerHealth>().dmgLeft(dmgAmount,this);
+            // if (health <= 0 && /*particleEffects != null &&*/particleEffects.Length > 0)
+            // {
+            //     RpcExploding();
+            //     // GameObject explosion = Instantiate(particleEffects[0], transform.position, Quaternion.identity);
+            //     // Destroy(transform.parent.gameObject);
+            //     // Destroy(explosion, 3f);
+            // }
         }
         if (hitboxType == Hitbox.RightHitbox)
         {
-            Debug.Log("Right takes damage"); health -= dmgAmount;
-                if (health <= 0 && /*particleEffects != null &&*/particleEffects.Length > 0)
-                {
-                RpcExploding();
-                // GameObject explosion = Instantiate(particleEffects[0], transform.position, Quaternion.identity);
-                // Destroy(transform.parent.gameObject);
-                // Destroy(explosion, 3f);
-            }
+            Debug.Log("Right takes damage");             
+            transform.parent.parent.GetComponent<PlayerHealth>().dmgRight(dmgAmount,this);
+            //     if (health <= 0 && /*particleEffects != null &&*/particleEffects.Length > 0)
+            //     {
+            //     // RpcExploding();
+            //     // GameObject explosion = Instantiate(particleEffects[0], transform.position, Quaternion.identity);
+            //     // Destroy(transform.parent.gameObject);
+            //     // Destroy(explosion, 3f);
+            // }
 
 
         }
@@ -72,35 +72,52 @@ public class MechTakeDamage : NetworkBehaviour, IDamagable
         {
             Debug.Log("Core takes " + dmgAmount + " damage");
             
-            health -= dmgAmount;
+            coreDestroyed=transform.parent.parent.GetComponent<PlayerHealth>().dmgCore(dmgAmount,this);
             GameObject flames = Instantiate(particleEffects[1], transform.position, Quaternion.identity, transform);
             flames.transform.localScale += new Vector3(1f, 1f, 1f);
-            if (health <= 0)
-            {
-                coreDestroyed = true;
-                RpcExplodingCore();
+            // if (health <= 0)
+            // {
+
+                // RpcExplodingCore();
                 // GameObject explosion = Instantiate(particleEffects[0], transform.position, Quaternion.identity);
                 // explosion.transform.localScale += new Vector3(1f, 1f, 1f);
                 // Destroy(explosion, 3f);
                 // Destroy(transform.parent.gameObject);
-            }
+            // }
         }
 
         if (hitboxType == Hitbox.RearHitbox)
         {
             Debug.Log("Rear takes damage");
-
-            health -= dmgAmount;
-            if (health <= 0 && particleEffects != null && particleEffects.Length > 0)
-            {
-                RpcExploding();
+            transform.parent.parent.GetComponent<PlayerHealth>().dmgRear(dmgAmount,this);
+            // if (health <= 0 && particleEffects != null && particleEffects.Length > 0)
+            // {
+            //     RpcExploding();
                 // GameObject explosion = Instantiate(particleEffects[0], transform.position, Quaternion.identity);
                 // Destroy(transform.parent.gameObject);
                 // Destroy(explosion, 3f);
-            }
+            // }
         }
 
         return coreDestroyed;
+
+    }
+    public void explodeServer()
+    {
+        if(particleEffects != null && particleEffects.Length > 0)
+        {
+            if (hitboxType == Hitbox.CoreHitbox)
+            {
+            RpcExplodingCore();
+            }
+            else if(hitboxType == Hitbox.FrontHitbox)
+            {
+             RpcExplodingFront();
+            }
+            else{
+                RpcExploding();
+            }
+        }
 
     }
     [ClientRpc]
---
+++ Assets/Scripts/Damage/PlayerHealth.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerHealth : NetworkBehaviour
+{
+    // Start is called before the first frame update
+    [SyncVar]public int coreHealth,rearHealth,leftHealth,rightHealth,frontHealth;
+
+public bool dmgCore(int dmg, MechTakeDamage hitbox)
+{
+    coreHealth-=dmg;
+    if(coreHealth<0)
+    {
+        hitbox.explodeServer();
+        return true;
+    }
+    return false;
+}
+public void dmgFront(int dmg, MechTakeDamage hitbox)
+{
+    frontHealth-=dmg;
+    
+    if(frontHealth<0)
+    {
+        hitbox.explodeServer();
+    }
+}
+public void dmgRear(int dmg, MechTakeDamage hitbox)
+{
+    rearHealth-=dmg;
+        if(rearHealth<0)
+    {
+        hitbox.explodeServer();
+    }
+}
+public void dmgLeft(int dmg, MechTakeDamage hitbox)
+{
+    leftHealth-=dmg;
+        if(leftHealth<0)
+    {
+        hitbox.explodeServer();
+    }
+}
+public void dmgRight(int dmg, MechTakeDamage hitbox)
+{
+    rightHealth-=dmg;
+        if(rightHealth<0)
+    {
+        hitbox.explodeServer();
+    }
+}
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
