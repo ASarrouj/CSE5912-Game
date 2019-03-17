a3fe70b478feb9bfd0799940207efa478948310f
Ian Pennington <irpennin@gmail.com>
2019-03-17 01:44:51 -0400

    added volume options to in-game menu

--- Assets/Scripts/PongScripts/MenuScripts/VolumeControl.cs
+++ Assets/Scripts/PongScripts/MenuScripts/VolumeControl.cs
@@ -4,26 +4,28 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class VolumeControl : MonoBehaviour
-{
-    public UnityEngine.Audio.AudioMixer mixer;
-    public string AudioGroup;
+{ 
+    public UnityEngine.Audio.AudioMixerGroup output;
     public Slider slider;
 
+    private UnityEngine.Audio.AudioMixer mixer;
     private float newVol;
 
     // Start is called before the first frame update
     void Start()
     {
+        mixer = output.audioMixer;
         slider.minValue = 0;
         slider.maxValue = 100;
-        slider.value = GlobalVariables.MasterVolume + 80;
+        float vol;
+        mixer.GetFloat(output.name, out vol);//GlobalVariables.MasterVolume + 80;
+        slider.value = vol + 80;
     }
 
     // Update is called once per frame
     void Update()
     {
         newVol = slider.value - 80;
-        mixer.SetFloat(AudioGroup, newVol);
-        GlobalVariables.MasterVolume = newVol;
+        mixer.SetFloat(output.name, newVol);
     }
 }
