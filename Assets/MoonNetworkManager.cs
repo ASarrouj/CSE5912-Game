bc9da76f91c37e6538cb69c40a5da7e58ee20bf3
clinton-23 <35583413+clinton-23@users.noreply.github.com>
2019-03-25 23:34:16 -0400

    forgot to add script to previous commit

---
+++ Assets/MoonNetworkManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+public class MoonNetworkManager : NetworkManager
+{
+    public override void OnServerAddPlayer(NetworkConnection conn, short playerControllerId)
+    {
+        TerrainGenerator tg = GameObject.Find("Terrain").GetComponent<TerrainGenerator>();
+
+        GameObject player = (GameObject)Instantiate(playerPrefab, Vector3.zero, Quaternion.identity);
+        NetworkServer.AddPlayerForConnection(conn, player, playerControllerId);
+
+        Vector3 pos = tg.getSpawnPoint();
+        player.transform.position = pos;
+    }
+}
