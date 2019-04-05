4702460f753b110244d031895ab0a39c5e061535
Nar Wang <31118588+narlynar@users.noreply.github.com>
2019-04-05 04:24:18 -0400

    Other Gun a work in progress. Art textured for now

---
+++ Assets/Scripts/Input/ArtilleryRotation.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArtilleryRotation
+{
+    // Start is called before the first frame update
+    public Transform weapon;
+    Quaternion originalRotation;
+    float sensitive=1.0f;
+        public ArtilleryRotation(Transform weapon)
+    {
+        this.weapon = weapon;
+        Rigidbody rb = weapon.GetComponent<Rigidbody>();
+        if (rb)
+            rb.freezeRotation = true;
+        originalRotation = weapon.localRotation;
+    }
+    public void Update(PlayerInput.InputType inputType)
+    {
+
+    }
+}
