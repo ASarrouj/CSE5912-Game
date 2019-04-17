84eb2be3a9f01d75a6a481bfb200fd0b9f1022e8
Patrick Capps <patrick.capps0@gmail.com>
2019-04-16 21:08:01 -0400

    New object destory script for networking

---
+++ Assets/Scripts/Utility/NetworkObjectDestroy.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class NetworkObjectDestroy : NetworkBehaviour
+{
+    public float existTime = 2f;
+
+    private void OnEnable()
+    {
+        Invoke("Destroy", existTime);
+    }
+
+    [Command]
+    void CmdDestroy()
+    {
+        Destroy();
+        RpcDestroy();
+    }
+
+    [ClientRpc]
+    void RpcDestroy()
+    {
+        Destroy();
+    }
+
+    private void Destroy()
+    {
+        gameObject.SetActive(false);
+    }
+
+    private void OnDisable()
+    {
+        CancelInvoke();
+    }
+}
