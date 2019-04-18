7f9e86ef81407d66ac532b585b97ae913a4a5613
clinton-23 <35583413+clinton-23@users.noreply.github.com>
2019-04-18 00:12:40 -0400

    trying to get it working

--- Assets/Scripts/MachineGunSync.cs
+++ Assets/Scripts/MachineGunSync.cs
@@ -5,10 +5,11 @@ using UnityEngine.Networking;
 
 public class MachineGunSync : NetworkBehaviour
 {
+    private ObjectPooler roundPool;
     // Start is called before the first frame update
     void Start()
     {
-        
+        roundPool = GameObject.Find("ArtProjectilePool").GetComponent<ObjectPooler>();
     }
 
     // Update is called once per frame
@@ -69,5 +70,26 @@ public class MachineGunSync : NetworkBehaviour
         {
             gameObject.GetComponentInChildren<RayCastShoot>().ShotHit(hit_point);
         }
+    }
+
+    [Command]
+    public void CmdSpawnProjectile(Vector3 position, Quaternion rotation, Vector3 forward, float projectileForce)
+    {
+        GameObject newProjectile = roundPool.GetObject();
+
+        newProjectile.transform.position = position + forward * .4f;
+        newProjectile.transform.rotation = rotation;
+        newProjectile.transform.Rotate(new Vector3(90, 0, 0));
+        newProjectile.SetActive(true);
+        newProjectile.GetComponent<Rigidbody>().velocity = new Vector3(0f, 0f, 0f);
+        newProjectile.GetComponent<Rigidbody>().AddForce(forward * projectileForce, ForceMode.Impulse);
+
+        NetworkServer.Spawn(newProjectile);
+    }
+
+    [Command]
+    public void CmdArtilleryExplosion(Vector3 position)
+    {
+        ;
     }
 }
