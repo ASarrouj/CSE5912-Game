3d896a498be6b514fdbf2c2e2249c29f2ac55b04
Patrick Capps <patrick.capps0@gmail.com>
2019-04-18 00:04:16 -0400

    new script is networkbehavior

--- Assets/Scripts/Utility/NetworkObjectDisable.cs
+++ Assets/Scripts/Utility/NetworkObjectDisable.cs
@@ -1,8 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
-public class NetworkObjectDisable : MonoBehaviour
+public class NetworkObjectDisable : NetworkBehaviour
 {
     public float existTime = 2f;
 
