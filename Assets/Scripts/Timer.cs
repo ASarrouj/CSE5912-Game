906363d620af56c017d96670755adb307003b3e3
ASarrouj <asarrouj590@gmail.com>
2019-04-18 13:50:18 -0400

    added timer to player UI

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public Text timerText;
+
+    private int roundLength;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        roundLength = 180;
+    }
+
+    void Update()
+    {
+        timerText.text = "Time: " + ((int)Mathf.Ceil(roundLength - Time.time) / 60).ToString() + ":" + ((int)Mathf.Ceil(roundLength - Time.time) % 60).ToString();
+    }
+}
